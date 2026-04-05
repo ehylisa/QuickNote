5bcdd119c10251e302bd9ee979252eb9012eb47f
Lisa <lisa.eunhye@gmail.com>
2026-04-05 14:00:40 -0400

    Initial Commit

---
+++ QuickNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickNotes
+//
+//  Created by Lisa Yi on 2026-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickNotes/QuickNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNotesApp.swift
+//  QuickNotes
+//
+//  Created by Lisa Yi on 2026-04-05.
+//
+
+import SwiftUI
+
+@main
+struct QuickNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
